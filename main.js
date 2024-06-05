9a8f257792d27d17728c4e5da98de514a5ad2635
Zoha Nafees <zohanafees09@gmail.com>
2024-06-06 00:07:12 +0500

    first module practice

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const module_js_1 = require("./module.js");
+const module_js_2 = require("./module.js");
+let result1 = (0, module_js_1.add)(12, 5);
+let result2 = (0, module_js_2.subtract)(10, 3);
+console.log(result1);
+console.log(result2);
---
+++ main.ts
@@ -0,0 +1,6 @@
+import {add} from './module.js'
+import {subtract} from './module.js'
+let result1=add(12,5)
+let result2=subtract(10,3)
+console.log(result1)
+console.log(result2)
